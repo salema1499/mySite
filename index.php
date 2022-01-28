ea7b2e935ba96fd57552cd9f8198e600ff698740
MKhaled <mkhaled@git-client.hoopoe.com>
2022-01-29 01:20:44 +0200

    edit title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible " content="ie=edge ">
-    <title>bootstrap</title>
+    <title>My Fans</title>
 
     <link rel="stylesheet " href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css " integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l " crossorigin="anonymous ">
     <link rel="stylesheet " href="css/style.css ">
